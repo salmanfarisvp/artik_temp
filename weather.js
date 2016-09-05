6cbd542dc184b1a8770cafc961409d4b12f198f8
Salman Faris <farissalmannbr@gmail.com>
2016-09-05 22:44:09 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 var artikcloud = "https://api.artik.cloud/v1.1/messages";
-var bearer = "Bearer INSERT_DEVICE_TOKEN_HERE";
-var sdid = "INSERT_DEVICE_ID_HERE";
+var bearer = "Bearer INSERT_DEVICE_TOKEN_HERE"; //bc5791dff4364280903199ba5d0c620f
+var sdid = "INSERT_DEVICE_ID_HERE"; //542d8acd601a43cd9585c17ed51b2960
 
 var serialport = require("serialport")
 var SerialPort = serialport.SerialPort;
